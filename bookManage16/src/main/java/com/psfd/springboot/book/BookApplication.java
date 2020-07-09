7cf35f0155e0a626ef3bdd9674f44db649be21da
LuYangya <2948758821@qq.com>
2020-07-09 16:50:25 +0800

    first

--- bookManage16/src/main/java/com/psfd/springboot/book/BookApplication.java
+++ bookManage16/src/main/java/com/psfd/springboot/book/BookApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BookApplication {
 
     public static void main(String[] args) {
+        System.out.println("dwa");
         SpringApplication.run(BookApplication.class, args);
     }
 

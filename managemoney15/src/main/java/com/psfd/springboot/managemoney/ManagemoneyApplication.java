0caff4727d759106cad341527f2369606249c745
zhanghao <1944676302@qq.com>
2020-07-09 16:04:12 +0800

    第一次提交

---
+++ managemoney15/src/main/java/com/psfd/springboot/managemoney/ManagemoneyApplication.java
@@ -0,0 +1,13 @@
+package com.psfd.springboot.managemoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagemoneyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagemoneyApplication.class, args);
+    }
+
+}
---
+++ managemoney15/src/test/java/com/psfd/springboot/managemoney/ManagemoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psfd.springboot.managemoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagemoneyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

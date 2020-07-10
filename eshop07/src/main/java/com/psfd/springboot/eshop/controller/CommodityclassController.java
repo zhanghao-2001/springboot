b6acff6f77b2e7b5bec1107da39b96f226a152b9
rootyrb <123@qq.com>
2020-07-10 20:46:36 +0800

    2020-7-10 eshop第5次提交

--- eshop07/src/main/java/com/psfd/springboot/eshop/controller/CommodityclassController.java
+++ eshop07/src/main/java/com/psfd/springboot/eshop/controller/CommodityclassController.java
@@ -1,20 +1,34 @@
 package com.psfd.springboot.eshop.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.psfd.springboot.eshop.domain.Commodityclass;
+import com.psfd.springboot.eshop.service.ICommodityclassService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
  *
  * @author admin
  * @since 2020-07-10
  */
 @Controller
-@RequestMapping("/commodityclass")
 public class CommodityclassController {
+
+    @Autowired
+    private ICommodityclassService commodityclassService;
+
+    @RequestMapping("/addCommodityClass")
+    @ResponseBody
+    public String addCommodityClass(Commodityclass commodityclass) {
+//        commodityclassService.saveOrUpdate(commodityclass);
+        return "增加商品种类成功";
+    }
+
 
 }

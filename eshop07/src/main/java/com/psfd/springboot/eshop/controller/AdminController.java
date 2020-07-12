cf404ec6f72b192aee48cf38dcf93e56852f3eca
rootyrb <123@qq.com>
2020-07-12 17:07:02 +0800

    2020-7-12 eshop第2次提交

--- eshop07/src/main/java/com/psfd/springboot/eshop/controller/AdminController.java
+++ eshop07/src/main/java/com/psfd/springboot/eshop/controller/AdminController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpSession;
+
 /**
  * <p>
  * 前端控制器
@@ -27,21 +29,32 @@ public class AdminController {
 
     @RequestMapping("/login")
     @ResponseBody
-    public String login(Admin admin) {
+    public String login(Admin admin, HttpSession session) {
         System.out.println("admin = " + admin);
         QueryWrapper<Admin> wrapper = new QueryWrapper<>();
         wrapper.eq("username", admin.getUsername());
         wrapper.eq("password", admin.getPassword());
+        wrapper.select("admin_id");
         Integer count = adminService.count(wrapper);
+        Admin admin1 = adminService.getOne(wrapper);
+        session.setAttribute("adminId", admin1.getAdminId());
         return count.toString();
     }
 
     @RequestMapping("/updatePassword")
     @ResponseBody
-    public String updatePassword(Admin admin, String newPassword, String newPasswordOne) {
+    public String updatePassword(String password, String newPassword, String newPasswordOne, HttpSession session) {
+        QueryWrapper<Admin> wrapper = new QueryWrapper<>();
+        wrapper.eq("admin_id", session.getAttribute("adminId"));
+        Admin admin = adminService.getOne(wrapper);
         if (newPassword.equals(newPasswordOne)) {
-            admin.setPassword(newPassword);
-            return "修改密码成功";
+            if (admin.getPassword().equals(password)) {
+                admin.setPassword(newPassword);
+                adminService.saveOrUpdate(admin);
+                return "修改密码成功";
+            } else {
+                return "密码输入错误";
+            }
         } else {
             return "两次输入的密码不一致";
         }
